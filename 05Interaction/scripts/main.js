1c82df05579e5138b309549d6b331997ef348d7e
vankata99reload <45344687+vankata99reload@users.noreply.github.com>
2020-10-28 13:16:02 +0100

    test

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -1,18 +1,18 @@
 
-var rotationSpeed = 0.001;
+var rotationSpeed = 0.005;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
 
 
-// myOtherBox.addEventListener('mouseenter', function(){
-// 	rotationSpeed = 0.01;
-// 	console.log('mouse enter');
-// });
+ myOtherBox.addEventListener('mouseenter', function(){
+	rotationSpeed = 5;
+	console.log('mouse enter');
+ });
