d7492125a24cf5d768d79332963fa3bc765a40e9
vankata99reload <45344687+vankata99reload@users.noreply.github.com>
2020-11-03 00:53:18 +0100

    test

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -1,43 +1,43 @@
 
 var rotationSpeed = 0.001;
-var myOtherBox = document.getElementById('myOtherBox');
-
+var beer = document.getElementById('beer');
+var bourbon = document.getElementById('bourbon');
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
+	beer.object3D.rotation.x += rotationSpeed;
+	beer.object3D.rotation.y += rotationSpeed;
+	beer.object3D.rotation.z += rotationSpeed/2;
 	//console.log(myOtherBox.object3D.rotation);
 }
 setInterval(spin, 10);
 
 
-myOtherBox.addEventListener('mouseenter', function(){
+beer.addEventListener('mouseenter', function(){
 	rotationSpeed = 0.01;
 	console.log('entered');
 });
 
-myOtherBox.addEventListener('mouseleave', function(){
+beer.addEventListener('mouseleave', function(){
 	rotationSpeed = 0.001;
 	console.log('left');
 });
 
 
 
-  
+
 /*
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time.
  */
 
- var growspeed = 2 ;
+ var growspeed = 1.2 ;
 
  function grow(){
- 	myOtherBox.object3D.scale.x *= growspeed;
- 	myOtherBox.object3D.scale.y *= growspeed;
- 	myOtherBox.object3D.scale.z *= growspeed;
+ 	bourbon.object3D.scale.x *= growspeed;
+ 	bourbon.object3D.scale.y *= growspeed;
+ 	bourbon.object3D.scale.z *= growspeed;
  	//console.log(myOtherBox.object3D.scale);
  }
 
- myOtherBox.addEventListener('click', function(){ // uses a fuse
+ bourbon.addEventListener('click', function(){ // uses a fuse
  	grow();
 	console.log('grew');
  });
